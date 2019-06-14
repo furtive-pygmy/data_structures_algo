aaa847a4632ea39d3ba2c0923135c6975b7eb271
nowfel terki <nowfel.terki@tutanota.com>
2019-06-14 16:31:28 +0200

    add function to add element at the end

--- list/simple/implementation_1/list.h
+++ list/simple/implementation_1/list.h
@@ -7,6 +7,7 @@ typedef struct List {
 } List;
 List *create_list();
 int add_element_at_start(List *list, int v);
+int add_element_at_end(List *list, int v);
 #define list_head(list) ((list)->head)
 #define list_size(list) ((list)->size)
 #define list_is_empty(list) ((list)->size == 0 ? 1 : 0)
