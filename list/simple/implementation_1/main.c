d379490229f7242aa1842912625eb6cc8916db35
nowfel terki <nowfel.terki@tutanota.com>
2019-06-14 14:24:32 +0200

    change function call

--- list/simple/implementation_1/main.c
+++ list/simple/implementation_1/main.c
@@ -5,9 +5,9 @@
 int main(int argc, char *argv[])
 {
     List *list = create_list();
-    add(list, 12);
-    add(list, 22);
-    add(list, 32);
+    add_element_at_start(list, 12);
+    add_element_at_start(list, 22);
+    add_element_at_start(list, 32);
 
     return EXIT_SUCCESS;
 }
