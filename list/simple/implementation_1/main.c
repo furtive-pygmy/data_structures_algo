cc49d8a9ede6609f3ae4d9a757176d5a9c242e08
nowfel terki <nowfel.terki@tutanota.com>
2019-06-14 16:32:36 +0200

    call the function to add element at the end

--- list/simple/implementation_1/main.c
+++ list/simple/implementation_1/main.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     add_element_at_start(list, 12);
     add_element_at_start(list, 22);
     add_element_at_start(list, 32);
+    add_element_at_end(list, 99);
 
     return EXIT_SUCCESS;
 }
