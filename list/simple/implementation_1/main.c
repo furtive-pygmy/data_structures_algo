f14dfc962a6d64b3d957ee0a6ef5fc1f8b5b2d0d
nowfel terki <nowfel.terki@tutanota.com>
2019-06-19 20:00:43 +0200

    call this new function in main

--- list/simple/implementation_1/main.c
+++ list/simple/implementation_1/main.c
@@ -7,8 +7,7 @@ int main(int argc, char *argv[])
     List *list = create_list();
     add_element_at_start(list, 12);
     add_element_at_start(list, 22);
-    add_element_at_start(list, 32);
-    add_element_at_end(list, 99);
+    add_element_after_node(list, 42, 1);
 
     return EXIT_SUCCESS;
 }
