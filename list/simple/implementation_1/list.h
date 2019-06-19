e2afad9fb10832b5c3d5c1d5894c05cdf1bbd297
nowfel terki <nowfel.terki@tutanota.com>
2019-06-19 19:59:33 +0200

    add new function signature

--- list/simple/implementation_1/list.h
+++ list/simple/implementation_1/list.h
@@ -8,6 +8,7 @@ typedef struct List {
 List *create_list();
 int add_element_at_start(List *list, int v);
 int add_element_at_end(List *list, int v);
+int add_element_after_node(List *list, int v, int index);
 #define list_head(list) ((list)->head)
 #define list_size(list) ((list)->size)
 #define list_is_empty(list) ((list)->size == 0 ? 1 : 0)
