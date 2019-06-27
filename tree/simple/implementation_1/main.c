e8850222be6c7ffb34b1fcc24d9d2063617ca6cd
nowfel terki <nowfel.terki@tutanota.com>
2019-06-27 15:50:51 +0200

    create at tree

--- tree/simple/implementation_1/main.c
+++ tree/simple/implementation_1/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tree.h"
 
 int main(int argc, char *argv[])
 {
+	Tree *tree = (Tree*)malloc(sizeof(Tree));
+	tree->size = 0;
+	tree->root = NULL;
+	addValue(tree, 5);
+
 	return EXIT_SUCCESS;
 }
