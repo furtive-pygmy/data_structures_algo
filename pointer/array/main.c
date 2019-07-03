736072ba1ae1a5f244b7c20e11f9c87c11d7de2b
nowfel terki <nowfel.terki@tutanota.com>
2019-07-03 18:24:42 +0200

    add some example with 2d arrays and pointers

--- pointer/array/main.c
+++ pointer/array/main.c
@@ -17,5 +17,26 @@ int main(int argc, char *argv[])
     *(b+1) = 10; 
     *(b+2) = 11; 
 
+    int c[][2] = {
+        {99, 100},
+        {199, 200}
+    };
+
+    // We change the value of c by pointing on it
+    //*(*(pointer + i) + j)
+    int (*d)[2] = c;
+    *(*(d)) = 1000;//[0][0]
+    *(*(d + 1)) = 1001;//[1][0]
+
+    *(*(d) + 1) = 3000;//[0][1]
+    *(*(d + 1) + 1) = 4000;//[1][1]
+
+    // To know what is the address of n element in 2d array the formula is:
+    // m + B(i*c+j)
+    // where m is the memory_address, B the size in Bytes i the row index c the max number of column and j the column index
+    // so to know where is the address of the element [2][1]with an address of 140737488346748 for the first element we do as follow:
+    // 140737488346748 + 4(2*1 + 2)
+    // 140737488346764
+
     return EXIT_SUCCESS;
 }
